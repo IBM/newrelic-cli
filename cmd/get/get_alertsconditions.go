3ebaf21a00cbf31ceb5ff90b8c430a213b6de244
root <gail.tang@cn.ibm.com>
2021-04-29 02:10:49 -0500

    support multi location condition in get command

--- cmd/get/get_alertsconditions.go
+++ cmd/get/get_alertsconditions.go
@@ -104,6 +104,13 @@ var alertsconditionsCmd = &cobra.Command{
 					return
 				}
 				alertsConditionList = list
+				// location_failure_conditions uses different methods for pagination,
+				// if a page requested doesn't exist, it returns the first page instead of empty entires.
+				// use seperate logic to get location_failure_conditions, instead of adding it in ListAll
+				list, resp, err := client.AlertsConditions.List(context.Background(), conditionsOptions, newrelic.ConditionLocation)
+				if err == nil && resp.StatusCode >= 200 && resp.StatusCode <= 299 {
+					alertsConditionList.AlertsLocationConditionList = list.AlertsLocationConditionList
+				}
 			}
 		} else {
 			list, err := client.AlertsConditions.ListAll(context.Background(), conditionsOptions)
@@ -113,6 +120,13 @@ var alertsconditionsCmd = &cobra.Command{
 				return
 			}
 			alertsConditionList = list
+			// location_failure_conditions uses different methods for pagination,
+			// if a page requested doesn't exist, it returns the first page instead of empty entires.
+			// use seperate logic to get location_failure_conditions, instead of adding it in ListAll
+			list, resp, err := client.AlertsConditions.List(context.Background(), conditionsOptions, newrelic.ConditionLocation)
+			if err == nil && resp.StatusCode >= 200 && resp.StatusCode <= 299 {
+				alertsConditionList.AlertsLocationConditionList = list.AlertsLocationConditionList
+			}
 		}
 
 		if err != nil {
